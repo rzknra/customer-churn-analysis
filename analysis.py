2e12dacd20495403ec8390386052730315932a69
rzknra <rizky.nur.afifah@mail.ugm.ac.id>
2025-06-11 06:04:35 +0700

    feat: Add function to plot churn distribution

--- analysis.py
+++ analysis.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_data(file_path):
     """Loads data from a CSV file."""
     return pd.read_csv(file_path)
 
-print("Data loading function created.")
+def plot_churn_distribution(df):
+    """Plots the distribution of the churn column."""
+    df['churn'].value_counts().plot(kind='bar')
+    plt.title('Customer Churn Distribution')
+    plt.xlabel('Churn')
+    plt.ylabel('Count')
+    plt.show()
+
+print("Added data visualization function.")
